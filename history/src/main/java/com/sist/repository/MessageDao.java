0274aa0f74a660a23d8a0bd6e69a864b243475c5
Injae Lee <injlee@outlook.com>
2017-03-24 12:40:16 +0900

    @repository

--- history/src/main/java/com/sist/repository/MessageDao.java
+++ history/src/main/java/com/sist/repository/MessageDao.java
@@ -1,7 +1,10 @@
 package com.sist.repository;
 
+import org.springframework.stereotype.Repository;
+
 /**
  * Created by Admin on 2017-03-24.
  */
+@Repository
 public class MessageDao implements Dao {
 }

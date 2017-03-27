4fd988507cc94db287069e76df294c9f7edb3030
Hyun <schatz1234@naver.com>
2017-03-27 14:08:53 +0900

    파일명변경

--- history/src/main/java/com/hifive/history/service/LoveService.java
+++ history/src/main/java/com/hifive/history/service/LoveService.java
@@ -1,15 +1,43 @@
 package com.hifive.history.service;
 
+import com.hifive.history.model.iDto;
 import com.hifive.history.repository.LoveDao;
+
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-/**
- * Created by Admin on 2017-03-24.
- */
 @Service
-public class LoveService implements iService {
-
+public class LoveService implements iService{
+	
 	@Autowired
 	private LoveDao loveDao;
+
+	@Override
+	public int hi_insert(iDto dto) {
+		return 0;
+	}
+
+	@Override
+	public int hi_update(iDto dto) {
+		return 0;
+	}
+
+	@Override
+	public iDto hi_detail(iDto dto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int hi_delete(int cnt) {
+		return 0;
+	}
+
+	@Override
+	public List<Map<String, Object>> hi_selectPage(Map<String, Object> dto) throws Exception {
+		return null;
+	}
 }
--- history/src/main/java/com/hifive/history/service/PostService.java
+++ history/src/main/java/com/hifive/history/service/PostService.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
-public class PostIService implements iService {
+public class PostService implements iService {
 
 	@Autowired
 	private PostDao postDao;

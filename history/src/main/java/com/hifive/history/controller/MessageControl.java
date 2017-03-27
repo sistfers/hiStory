02eed748109d2929823aafba4d973cb7ac5f3ce1
Hyun <schatz1234@naver.com>
2017-03-27 11:18:25 +0900

    수정

--- history/src/main/java/com/hifive/history/controller/MessageControl.java
+++ history/src/main/java/com/hifive/history/controller/MessageControl.java
@@ -1,7 +1,10 @@
 package com.hifive.history.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by Admin on 2017-03-24.
  */
+@Controller
 public class MessageControl {
 }
--- history/src/main/java/com/hifive/history/repository/iDao.java
+++ history/src/main/java/com/hifive/history/repository/iDao.java
@@ -1,7 +1,18 @@
 package com.hifive.history.repository;
 
-/**
- * Created by Admin on 2017-03-23.
- */
+import java.util.List;
+import java.util.Map;
+
+import com.hifive.history.model.iDto;
+
 public interface iDao {
+	public int hi_insert(iDto dto);			// 등록
+	
+	public int hi_update(iDto dto);			// 수정
+	
+	public iDto hi_detail(iDto dto);		// 로그인,상세보기(1건읽기)
+	
+	public int hi_delete(int cnt);			// 삭제(3으로 상태변경)
+	
+	public List<Map<String, Object>> hi_selectPage(Map<String, Object> dto) throws Exception;		// 목록 리스트 보기
 }

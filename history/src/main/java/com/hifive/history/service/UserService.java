8170d2092e43050c19ba40484cab33d24e72ae12
Injae Lee <injlee@outlook.com>
2017-03-27 19:25:57 +0900

    user 수정

--- history/src/main/java/com/hifive/history/service/UserService.java
+++ history/src/main/java/com/hifive/history/service/UserService.java
@@ -23,28 +23,31 @@ public class UserService implements iService {
 
 	@Override
 	public int hi_insert(iDto dto) {
-		return 0;
+		return userDao.hi_insert(dto);
 	}
 
 	@Override
 	public int hi_update(iDto dto) {
-		return 0;
+		return userDao.hi_update(dto);
 	}
 
 	@Override
 	public iDto hi_detail(iDto dto) {
-		return null;
+		return userDao.hi_detail(dto);
+	}
+
+	public int hi_usercheck(Map<String, Object> condition) {
+		return userDao.hi_usercheck(condition);
 	}
 
 	@Override
 	public int hi_delete(int cnt) {
-		return 0;
+		return userDao.hi_delete(cnt);
 	}
 
 	@Override
 	public List<Map<String, Object>> hi_selectList(Map<String, Object> condition) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		return userDao.hi_selectList(condition);
 	}
 	
 	

b60aecbf75587e85d58a120f13ec40bc7fa5685b
PARK <PARK@thisis>
2017-03-29 15:07:47 +0900

    불필요한 import 삭제

--- history/src/main/java/com/hifive/history/controller/MessageControl.java
+++ history/src/main/java/com/hifive/history/controller/MessageControl.java
@@ -6,7 +6,6 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.jasper.tagplugins.jstl.core.Redirect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;

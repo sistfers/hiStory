d7603f401895a45e8d7662926fcf0e5603d3adb6
PARK <PARK@thisis>
2017-04-10 09:20:25 +0900

    소스 정리, 스레드

--- history/src/main/java/com/hifive/history/util/EmailSenderUtil.java
+++ history/src/main/java/com/hifive/history/util/EmailSenderUtil.java
@@ -52,43 +52,4 @@ public class EmailSenderUtil {
 		
 		return 0;
 	}
-
-	public ModelAndView eamilSender(UserDto dto, String token) {
-				
-		String seq = "";
-		
-		String sender  = "sisthifive@gmail.com";
-		String title   = "[Hifive] Please verify your email address";		
-		String content = "http://localhost:8080/verified/" + dto.getId() + "/" +  token +".hi";
-		
-		logger.debug("[[[content  ]] "+content);
-		
-//		try {
-//			content = URLEncoder.encode(content, "UTF-8");
-//			
-//		} catch (UnsupportedEncodingException e1) {
-//			e1.printStackTrace();
-//		}
-		
-		try {
-			MimeMessage message = mailSender.createMimeMessage();
-			MimeMessageHelper messageHelper 
-			= new MimeMessageHelper(message, true, "UTF-8");
-			
-			messageHelper.setFrom(sender);   		// 보내는사람 생략하거나 하면 정상작동을 안함
-		    messageHelper.setTo(dto.getEmail());    // 받는사람 이메일
-		    messageHelper.setSubject(title); 		// 메일제목은 생략이 가능하다
-		    messageHelper.setText(content); 		// 메일 내용
-		    
-		    mailSender.send(message);
-			
-		} catch (MessagingException e) {
-			e.printStackTrace();
-		}
-		
-		ModelAndView mav = new ModelAndView();
-		mav.setViewName("/auth/welcome");
-		
-		return mav;
-	}
 }

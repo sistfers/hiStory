7e2935c8fd4d2f83d93fb81dc984b72dd071aecb
YunHaengJo <dbsgod4289@hanmail.net>
2017-04-17 10:06:03 +0900

    성능테스트.java

---
+++ history/src/main/java/com/hifive/history/util/PerformMeasure.java
@@ -0,0 +1,48 @@
+package com.hifive.history.util;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@Aspect
+public class PerformMeasure {
+	 Logger log=LoggerFactory.getLogger(this.getClass());
+	 
+	 @Around("execution(* com..kl.*Controller.*(..))")
+	 public Object logging(ProceedingJoinPoint joinPoint) throws Throwable {
+		 
+		    String type = joinPoint.getSignature().getDeclaringTypeName();
+		    String name= joinPoint.getSignature().getName();
+		    log.debug("===================================type="+type);
+		    log.debug("===================================name="+name);
+		    System.out.println(name+"기록 시작:"+type);
+	        
+		    StopWatch stopWatch = new StopWatch();
+
+	        try {
+	            stopWatch.start();
+	            
+	            // 핵심 로직 실행.
+	            Object retValue = joinPoint.proceed();
+	            
+	            return retValue;
+	        } catch(Throwable e) {
+	            throw e;
+	        } finally {
+	            stopWatch.stop();
+	            System.out.println("기록 종료");
+	            log.debug("====================================");
+	            System.out.println(joinPoint.getSignature().getName() + "메서드 실행 시간 : " 
+	                + stopWatch.getTotalTimeMillis());
+	        }
+
+		 
+
+	 }
+
+}
